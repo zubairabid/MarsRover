34af0c6508eb2a006488d655b7c500062b562553
zubairabid <zubairabid1999+github@gmail.com>
2020-07-22 19:27:10 +0530

    A-star works. Need to sort out some quirks. Cleaner code

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,13 @@
+//////////////////////////
+//  functional/cell.js  //
+//////////////////////////
+
 class Cell {
-    constructor(i, j, start, end) {
+    constructor(i, j, start, end, level) {
         this.i = i;
         this.j = j;
-        this.level = 8;
+        this.level = level;
+        this.baseLevel = level;
         this.start = start;
         this.end = end;
         this.visited = false;
@@ -16,55 +21,36 @@ class Cell {
     }
 }
 
+function compareCells(a, b) {
+    if (a.score < b.score) return -1;
+    if (a.score > b.score) return 1;
+    return 0;
+}
+
+//////////////////////////
+//  functional/grid.js  //
+//////////////////////////
+
+// functional/grid.js
 class Grid {
-    constructor(rows, columns, starti, startj, endi, endj) {
+    constructor(rows, columns, starti, startj, endi, endj, level) {
         this.rows = rows;
         this.columns = columns;
         this.grid = [];
-        this.createGrid(starti, startj, endi, endj);
+        this.createGrid(starti, startj, endi, endj, level);
     }
 
-    createGrid(starti, startj, endi, endj) {
+    createGrid(starti, startj, endi, endj, level) {
         for (let i = 0; i < this.rows; i++) {
             let rowgrid = [];
             for (let j = 0; j < this.columns; j++) {
-                rowgrid.push(new Cell(i, j, (i==starti && j==startj), (i==endi && j==endj)));
+                let cell = new Cell(i, j, (i==starti && j==startj), (i==endi && j==endj), level);
+                paintCell(cell);
+                rowgrid.push(cell);
             }
             this.grid.push(rowgrid);
         }
     }
-}
-
-const INF = 1000
-
-function updateState(cell) {
-    options = ['visited', 'closed', 'path'];
-    elem = uiFromCell(cell);
-    elem.classList.remove(...options);
-    if (cell.path)
-        elem.classList.add('path');
-    else if (cell.closed)
-        elem.classList.add('closed');
-    else if (cell.visited)
-        elem.classList.add('visited');
-}
-
-function getCost(currentCell, neighbour, costFunction) {
-    cost = costFunction(currentCell, neighbour);
-    return cost;
-}
-
-function tempcost(currentCell, neighbour) {
-    let diff = currentCell.level - neighbour.level;
-    diff = Math.abs(diff);
-    diff += 1;
-    return diff>5?INF:diff;
-}
-
-function getHeuristic(cell, end) {
-    let d1 = Math.abs(cell.i - end.i);
-    let d2 = Math.abs(cell.j - end.j);
-    return d1+d2;
 }
 
 function getNeighbours(grid, cell) {
@@ -88,45 +74,172 @@ function getNeighbours(grid, cell) {
     return neighbours;
 }
 
-function astar(grid, start, end, costFunction) {
-    openList = [];
+const INF = 1000
+
+///////////////////////
+//  canvasHelper.js  //
+///////////////////////
+
+
+let levels = [];
+for (let i = 1; i <=16; i++) {
+    levels.push("l"+i);
+}
+
+let stateOptions = ['visited', 'closed'];
+
+// Takes an HTML div with id of format num-num and returns the corresponding
+// `cell` on the functional `grid`
+function cellFromUI(elem) {
+    let elemName = elem.id;
+    let rc = elemName.split('-');
+    let row = parseInt(rc[0]);
+    let col = parseInt(rc[1]);
+    return gridObject.grid[row][col];
+}
+
+// Takes a `cell` from the functional `grid` and returns the corresponding 
+// HTML div
+function uiFromCell(cell) {
+    let name = (cell.i)+'-'+(cell.j);
+    let elem = document.getElementById(name);
+    return elem;
+}
+
+// Takes a functional `cell` and sets the level
+function setLevel(cell, level) {
+    if (cell.level == level)
+        cell.level = cell.baseLevel;
+    else
+        cell.level = level;
+}
+
+// Takes a cell and updates the UI paint accordingly
+function paintCell(cell) {
+    paintStart(cell);
+    paintEnd(cell);
+    paintLevel(cell);
+    paintState(cell);
+}
+
+function paintStart(cell) {
+    if (cell.start) {
+        let elem = uiFromCell(cell);
+        elem.classList.add('start');
+    }
+}
+
+function paintEnd(cell) {
+    if (cell.end) {
+        let elem = uiFromCell(cell);
+        elem.classList.add('end');
+    }
+}
+
+function paintLevel(cell) {
+    let elem = uiFromCell(cell);
+
+    elem.classList.remove(...levels);
+    elem.classList.add("l"+cell.level);
+    //console.log(cell);
+}
+
+function paintState(cell) {
+    let elem = uiFromCell(cell);
+
+    elem.classList.remove(...stateOptions);
+    if (cell.closed)
+        elem.classList.add('closed');
+    else if (cell.visited)
+        elem.classList.add('visited');
+}
+
+// Paints out the final path
+function paintPath(path) {
+    for (let i = 0; i < path.length; i++) {
+        let cell = path[i];
+        let elem = uiFromCell(cell);
+        elem.classList.add('path');
+    }
+}
+
+////////////////////////
+//  end of that file  //
+////////////////////////
+
+/////////////////////////////////
+//  algorithms/pathHelpers.js  //
+/////////////////////////////////
+
+function getCost(currentCell, neighbour, costFunction) {
+    cost = costFunction(currentCell, neighbour);
+    return cost;
+}
+
+function tempcost(currentCell, neighbour) {
+    let diff = currentCell.level - neighbour.level;
+
+    let cost = 1;
+    if (diff > 0) {
+        cost = Math.pow(diff, 4);
+    }
+    else if (diff < 0){
+        diff = -1*diff;
+        cost = Math.pow(diff, 3)
+    }
+
+    if (diff > 4)
+        cost = INF;
+
+    return cost;
+}
+
+function manhattanDist(cell, end) {
+    let d1 = Math.abs(cell.i - end.i);
+    let d2 = Math.abs(cell.j - end.j);
+    return d1+d2;
+}
+
+///////////////////////////
+//  algorithms/astar.js  //
+///////////////////////////
+
+function astar(grid, start, end, costFunction, heuristicFunction, delay) {
+    // Initialise the blank "open list"
+    let openList = [];
     openList.push(start);
+
+    // counter is used to delay the timer
     let counter = 0;
     while (openList.length > 0) {
-        counter += 1;
-
         currentCell = openList[0];
         currentCell.closed = true;
-        setTimeout(()=>{updateState(currentCell);}, 20*counter);
-        //updateState(currentCell);
-        console.log("examining ", currentCell.i, currentCell.j, " at score ", currentCell.score);
+
+        // paint the cell 
+        setTimeout(()=>{paintState(currentCell);}, delay*counter);
 
         // If currentCell is final, return the successful path
         if (currentCell == end) {
             path = [];
             let curr = currentCell;
-            while (curr.parent != null) {
+            while (curr!= null) {
                 curr.path = true;
-                setTimeout(()=>{updateState(currentCell);}, 20*counter);
                 path.push(curr);
                 curr = curr.parent;
             }   
-            return path.reverse();
+            return path.reverse(), counter;
         }
 
         // push currentCell to closedList and remove from openList
         openList = openList.slice(1);
 
-        // TODO improve getNeighbours
         let neighbours = getNeighbours(grid, currentCell);
+
         // foreach neighbour of currentCell
-        console.log("neighbours: ", neighbours);
         for (let i = 0; i < neighbours.length; i++) {
             let neighbour = neighbours[i];
-            //console.log("with neighbour: ", neighbour);
 
             // Compute costs and distances
-            // TODO improve getCost
             let cost = getCost(currentCell, neighbour, costFunction);
             if (neighbour.closed || cost >= INF) {
                 continue;
@@ -139,12 +252,12 @@ function astar(grid, start, end, costFunction) {
             // gets is the "best" distance
             if (!neighbour.visited) {
                 bestDist = true;
-                // TODO improve getHeuristic
-                neighbour.heuristic = getHeuristic(neighbour, end);
+                neighbour.heuristic = heuristicFunction(neighbour, end);
                 neighbour.visited = true;
-                setTimeout(()=>{updateState(neighbour)}, 20*counter);
-                //updateState(neighbour);
-                // replace above with TODO updateVisited(neighbour, end)
+
+                // paint the cell
+                setTimeout(()=>{paintState(neighbour)}, delay*counter);
+                
                 openList.push(neighbour);
             }
             else if (dist < neighbour.dist) {
@@ -162,20 +275,15 @@ function astar(grid, start, end, costFunction) {
         // Update openList
         // TODO write the sort
         openList.sort(compareCells);
-
-
+        counter += 1;
     }
 
-    return [];
+    return [], counter;
 }
 
-
-function compareCells(a, b) {
-    if (a.score < b.score) return -1;
-    if (a.score > b.score) return 1;
-    return 0;
-}
-
+///////////////
+//  main.js  //
+///////////////
 
 
 // When the webpage is loaded for the first time, the board will be initialised
@@ -184,94 +292,70 @@ function compareCells(a, b) {
 // level is 8. Colour-coding of the levels will be defined in the CSS, and 
 // representation in HTML is as classes.
 
-let rows = 50, columns = 40;
+let BASE_LEVEL = 8;
+let rows = 0, columns = 0;
 let gridObject = null;
-
-// hardcoded for quick prototyping, refactor
-let starti = 34, startj = 4, endi = 4, endj = 44;
-
-window.onload = () => {
-    let grid = document.getElementsByClassName('gridrow')[0];
-    let classes = grid.classList;
-    rows = parseInt(classes[2]);
-    columns = parseInt(classes[3]);
-
-    gridObject = new Grid(rows, columns, starti, startj, endi, endj);
-}
-
-// Adding the start/stop to the HTML board. It already exists on representation
-let temp = document.getElementById(starti+'-'+startj);
-temp.classList.add('start')
-temp = document.getElementById(endi+'-'+endj);
-temp.classList.add('end')
-
-
 
 // Variables that help define actions when the mouse moves over the nodes in
 // the grid
 let level = 14;
 let painting = false;
 
-// Random variables atm
-let levels = [];
-for (let i = 1; i <=16; i++) {
-    levels.push("l"+i);
+let starti = 0, startj = 0, endi = 0, endj = 0;
+
+window.onload = () => {
+    let grid = document.getElementsByClassName('gridrow')[0];
+    let classes = grid.classList;
+
+    // Get the number of rows
+    rows = parseInt(classes[2]);
+    columns = parseInt(classes[3]);
+
+    // hardcoded for quick prototyping, refactor
+    starti = 34;
+    startj = 4;
+    endi = 4;
+    endj = 44;
+
+    gridObject = new Grid(rows, columns, starti, startj, endi, endj, BASE_LEVEL);
 }
+
 // Event listeners for all nodes. When the cell style is updated, so is
 // the node in the JavaScript representation
-window.addEventListener('mousedown', e => {
-    painting = true;
-    console.log('mousedown! pressed = ', painting);
-});
-
 window.addEventListener('mouseup', e => {
     painting = false;
     console.log('mouseup! pressed = ', painting);
 });
 
-
 for (let i = 0; i < 40; i++) {
     for (let j = 0; j < 50; j++) {
         let mouseTarget = document.getElementById(i+'-'+j);
+
+        mouseTarget.addEventListener('mousedown', e => {
+            painting = true;
+            console.log('mousedown! pressed = ', painting);
+        });
+
         mouseTarget.addEventListener('click', e => {
-            defineLevel(mouseTarget);
+            let cell = cellFromUI(mouseTarget);
+            setLevel(cell, level);
+            paintCell(cell);
         })
 
         mouseTarget.addEventListener('mouseenter', e => {
             if (painting) {
                 console.log("Mouse entered ", mouseTarget.id, 'while clicked');
-                defineLevel(mouseTarget);
+                let cell = cellFromUI(mouseTarget);
+                setLevel(cell, level);
+                paintCell(cell);
             }
         });
     }
 }
 
-function cellFromUI(elem) {
-    let elemName = elem.id;
-    let rc = elemName.split('-');
-    let row = parseInt(rc[0]);
-    let col = parseInt(rc[1]);
-    return gridObject.grid[row][col];
-}
-
-function uiFromCell(cell) {
-    let name = (cell.i)+'-'+(cell.j);
-    let elem = document.getElementById(name);
-    return elem;
-}
-
-function defineLevel(elem) {
-    let cell = cellFromUI(elem);
-
-    if (elem.classList.contains("l"+level)) {
-        elem.classList.remove("l"+level);
-        elem.classList.add("l8");
-        cell.level = 8;
-    }
-    else {
-        elem.classList.remove(...levels);
-        elem.classList.add("l"+level);
-        cell.level = level;
-    }
-    console.log(cell);
+function run(delay) {
+    let start = gridObject.grid[starti][startj];
+    let end = gridObject.grid[endi][endj];
+    let path, timedelay = astar(gridObject.grid, start, end, tempcost, manhattanDist, delay);
+    setTimeout(()=>{paintPath(path);}, timedelay*delay);
 }
