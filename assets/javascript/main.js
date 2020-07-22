226badf6febb3e3eaa9079cbbf317fa8332e1784
zubairabid <zubairabid1999+github@gmail.com>
2020-07-22 13:12:41 +0530

    Non-functioning skeleton for a-star

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,6 +5,13 @@ class Cell {
         this.level = 8;
         this.start = start;
         this.end = end;
+        this.visited = false;
+        this.closed = false;
+        this.parent = null;
+
+        this.distance = 0;
+        this.heuristic = 0;
+        this.score = 0;
     }
 }
 
@@ -43,8 +50,70 @@ class Grid {
      // }
  // }
 
+const INF = 1000
 
-function astar(grid, start, end) {
+function astar(grid, start, end, costFunction) {
+    openList = [];
+    openList.push(start);
+    while (openList.length > 0) {
+        currentCell = openList[0];
+
+        // If currentCell is final, return the successful path
+        if (currentCell == end) {
+            path = [];
+            let curr = currentCell;
+            while (curr.parent != null) {
+                path.push(curr);
+                curr = curr.parent;
+            }   
+            return path.reverse();
+        }
+
+        // push currentCell to closedList and remove from openList
+        openList = openList.slice(1);
+
+        let neighbours = getNeighbours(grid, currentCell);
+        // foreach neighbour of currentCell
+        for (let i = 0; i < neighbours.length; i++) {
+            let neighbour = neighbours[i];
+
+            // Compute costs and distances
+            // TODO write getCost
+            let cost = getCost(currentCell, neighbour, costFunction);
+            if (neighbour.closed || cost > INF) {
+                continue;
+            }
+
+            let dist = currentCell.distance + cost;
+            let bestDist = false;
+
+            // If the neighbour hasn't been visited, whichever distance it
+            // gets is the "best" distance
+            if (!neighbour.visited) {
+                bestDist = true;
+                // TODO write getHeuristic
+                neighbour.heuristic = getHeuristic(neighbour, end);
+                neighbour.visited = true;
+                openList.push(neighbour);
+            }
+            else if (dist < neighbour.dist) {
+                bestDist = true;
+            }
+
+            // If it is the best, update the neighbour's distance/score/parent
+            if (bestDist) {
+                neighbour.distance = dist;
+                neighbour.parent = currentCell;
+                neighbour.score = neighbour.distance + neighbour.heuristic;
+            }
+        }
+        
+        // Update openList
+        // TODO write the sort
+        sort(openList);
+    }
+
+    return [];
 }
 
 
@@ -90,20 +159,20 @@ for (let i = 1; i <=16; i++) {
 }
 // Event listeners for all nodes. When the cell style is updated, so is
 // the node in the JavaScript representation
+window.addEventListener('mousedown', e => {
+    painting = true;
+    console.log('mousedown! pressed = ', painting);
+});
+
+window.addEventListener('mouseup', e => {
+    painting = false;
+    console.log('mouseup! pressed = ', painting);
+});
+
 
 for (let i = 1; i <= 40; i++) {
     for (let j = 1; j <= 50; j++) {
         let mouseTarget = document.getElementById(i+'-'+j);
-        mouseTarget.addEventListener('mousedown', e => {
-            painting = true;
-            console.log('mousedown! pressed = ', painting);
-        });
-
-        mouseTarget.addEventListener('mouseup', e => {
-            painting = false;
-            console.log('mouseup! pressed = ', painting);
-        });
-
         mouseTarget.addEventListener('click', e => {
             defineLevel(mouseTarget);
         })
